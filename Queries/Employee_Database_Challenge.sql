2b20f63a3d3ab1fa79d4821c53dacfd47b4b149d
Fenwayb <fenwayb@gmail.com>
2022-07-30 14:59:13 -0400

    Assignment upload

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,33 @@
+SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+INTO retirement_titles
+FROM Employees AS e
+JOIN Titles as t on e.emp_no = t.emp_no
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY e.emp_no
+;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, emp_no DESC;
+
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.title
+INTO mentorship_eligibility
+FROM employees AS e
+JOIN dept_emp AS de ON e.emp_no = de.emp_no
+JOIN titles AS t ON e.emp_no = t.emp_no
+WHERE de.to_date = '9999-01-01' 
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no;
